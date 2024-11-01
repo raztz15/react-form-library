01455fe5ec92446e8044d2703fb91c6e53e18759
raztz15 <raztz15@gmail.com>
2024-11-01 16:12:46 +0200

    Creating the form using its interface and basic styling && focusing in the first input on first render

--- src/components/Form.tsx
+++
@@ -1,4 +0,0 @@
-export const Form = () => {
-
-    return <div></div>
-}
---
+++ src/components/form/Form.tsx
@@ -0,0 +1,31 @@
+import { useRef } from 'react'
+import { IForm } from '../../interfaces'
+import './Form.css'
+export const Form = ({ inputs, buttons }: IForm) => {
+
+    const inputRef = useRef<HTMLInputElement>(null)
+
+    const handleChange = () => {
+
+    }
+
+    return <div className="form--container">
+        <div className='inputs--container'>
+            {inputs.map(({ label, inputType: type, id, validation, required }, index) => <div key={id}>
+                <label htmlFor={id}>{label}: </label>
+                <input
+                    type={type}
+                    id={id}
+                    name={id}
+                    required={required}
+                    onChange={handleChange}
+                    pattern={validation?.regex.source}
+                    ref={index === 0 ? inputRef : undefined}
+                />
+            </div>)}
+            <div className="form-buttons--container" >
+                {buttons.map(({ buttonType: type, text }) => <button key={text} type={type}>{text}</button>)}
+            </div>
+        </div>
+    </div>
+}
