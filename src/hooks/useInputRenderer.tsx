1af23a7797b14b2cc270543a18e16cc5f82523b5
raztz15 <raztz15@gmail.com>
2024-11-02 01:17:03 +0200

    Removing comments

--- src/hooks/useInputRenderer.tsx
+++ src/hooks/useInputRenderer.tsx
@@ -76,50 +76,5 @@ export function useInputRenderer({ inputsGroups, firstInput, errors, handleChang
         })}
     </div>)
 
-    // const renderInputs = inputs.map(({ inputType, options, required, id, validation, label, defaultValue }) => {
-    //     switch (inputType) {
-    //         case InputType.Radio:
-    //             return <div key={id} className="radio-input">
-    //                 <label htmlFor={id}>{label}:</label>
-    //                 <div>{options?.map(option => <div key={option} className="radio-option">
-    //                     <input
-    //                         type={inputType}
-    //                         id={`${id}--${option}`}
-    //                         name={id}
-    //                         value={option}
-    //                         required={required}
-    //                         onChange={handleChange}
-    //                         checked={defaultValue === option}
-    //                     />
-    //                     <label htmlFor={`${id}--${option}`}>{option}</label>
-    //                 </div>)}</div>
-    //             </div>
-
-    //         case InputType.Select:
-    //             return <div key={id}>
-    //                 <label htmlFor={id}>{label}:</label>
-    //                 <select id={id} name={id} onChange={handleChange}>
-    //                     {options?.map(option => <option key={option} value={defaultValue}>{option}</option>)}
-    //                 </select>
-    //             </div>
-
-    //         default:
-    //             return <div key={id}>
-    //                 <label htmlFor={id}>{label}:</label>
-    //                 <input
-    //                     value={defaultValue}
-    //                     type={inputType}
-    //                     id={id}
-    //                     name={id}
-    //                     required={required}
-    //                     onChange={handleChange}
-    //                     pattern={validation?.regex?.source}
-    //                     ref={firstInput.id === id ? inputRef : undefined}
-    //                     style={{ borderColor: errors[id] ? 'red' : '' }}
-    //                 />
-    //                 {errors[id] && <div className='input-error-message'>{validation?.errorMessage}</div>}
-    //             </div>
-    //     }
-    // })
     return renderInputsGroups
 }
