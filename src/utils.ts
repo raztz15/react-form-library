1b2d152ee6e1c82a31c95196b21303aa1e4fe224
raztz15 <raztz15@gmail.com>
2024-11-02 17:38:01 +0200

    Adding comments to utils file

--- src/utils.ts
+++ src/utils.ts
@@ -1,14 +1,17 @@
 import { IInpuitsGroups, IInput } from "./interfaces";
 
+// Utility function to retrieve all nested input fields from input groups
 export function getAllNestedInputs(inputsGroups: IInpuitsGroups[]) {
-    const newArr: IInput[] = []
+    const newArr: IInput[] = []; // Array to hold all nested inputs
 
+    // Iterate over each input group
     inputsGroups.forEach(group => {
-        const { inputs } = group
+        const { inputs } = group; // Destructure inputs from the group
+        // Check if inputs is an array and concatenate it to newArr
         if (Array.isArray(inputs)) {
-            newArr.push(...inputs)
+            newArr.push(...inputs);
         }
-    })
+    });
 
-    return newArr
+    return newArr; // Return the accumulated array of inputs
 }
